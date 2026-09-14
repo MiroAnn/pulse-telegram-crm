dbabb6514705c02a2aa88eea88cc58b3196ad6d5
Ann <ann@Annas-MacBook-Pro.local>
2026-09-14 16:06:07 +0300

    Update course tariffs link

--- app/api/telegram/update/route.ts
+++ app/api/telegram/update/route.ts
@@ -24,7 +24,7 @@ type TelegramUpdate = {
   };
 };
 
-const TARIFFS_URL = "https://miroann.github.io/zdorovaya-osanka-darya/tarifs";
+const TARIFFS_URL = "https://course.dariakavunenko.ru/tarifs/";
 const POSTURE_GUIDE_URL = "https://www.dropbox.com/scl/fi/3xyvw111dp1pzsai69imt/.pdf?rlkey=pkezbjoe1bb0rg3ghphnu3tod&dl=0";
 const WEBINAR_MESSAGE = `Здравствуйте!
 
